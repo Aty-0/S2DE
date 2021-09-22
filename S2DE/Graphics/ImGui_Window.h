237fa750cf6b6368d6e1e3fe6e43716e08c2fe38
Antd23rus <danich-anton-live@yandex.ru>
2021-09-22 16:30:22 +0300

    Add

---
+++ S2DE/Graphics/ImGui_Window.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Base/Main/Common.h"
+#include "Graphics/Renderer.h"
+
+
+//It's not thing from imgui library it's just simple base class for
+//classes where I use only ImGui and I need add it to render
+//in renderer I add a std::vector<ImGui_Window> and i render all his items 
+
+namespace S2DE
+{
+	class S2DE_API ImGui_Window
+	{
+	public:
+		ImGui_Window();
+		~ImGui_Window();
+
+		virtual void Render() = 0;
+		virtual void TougleDraw();
+		inline bool  isDrawing() const { return m_draw; }
+		
+	protected:
+		bool		 m_draw;
+	};
+}
