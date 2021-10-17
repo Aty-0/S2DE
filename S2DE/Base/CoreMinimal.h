d5b5ca76c1b26b36d634dd7098fc523dd5b87a21
Antd23rus <danich-anton-live@yandex.ru>
2021-10-17 13:06:13 +0300

    Update

--- S2DE/Base/CoreMinimal.h
+++ S2DE/Base/CoreMinimal.h
@@ -1,6 +1,30 @@
 #pragma once
 #include "Base\Engine.h"
 #include "Base\ResourceManager.h"
+#include "Base\GameWindow.h"
+
+//////////////////////////////////////////
 #include "Graphics\Renderer.h"
 #include "Graphics\Buffers.h"
+#include "Graphics\Texture.h"
 #include "Graphics\Shader.h"
+
+//////////////////////////////////////////
+#include "GameObjects/GameObject.h"
+#include "GameObjects/Sprite.h"
+
+
+//////////////////////////////////////////
+#include "Scene/Scene.h"
+#include "Scene/SceneManager.h"
+
+//////////////////////////////////////////
+#include "Math/Clamp.h"
+#include "Math/Color.h"
+#include "Math/MathConstants.h"
+#include "Math/Matrix.h"
+#include "Math/Max.h"
+#include "Math/Min.h"
+#include "Math/Random.h"
+#include "Math/Rect.h"
+#include "Math/Vector.h"
