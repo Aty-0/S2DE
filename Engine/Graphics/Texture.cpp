6968bab813146197d09d4e1881f6fcf952b852a3
Antd23rus <danich-anton-live@yandex.ru>
2022-01-22 20:34:10 +0300

    Now we use WICTextureLoader and DDSTextureLoader

--- Engine/Graphics/Texture.cpp
+++ Engine/Graphics/Texture.cpp
@@ -2,7 +2,8 @@
 #include "Base/Engine.h"
 #include "Graphics/Renderer.h"
 
-#include <D3DX11tex.h>
+#include <WICTextureLoader.h>
+#include <DDSTextureLoader.h>
 
 namespace S2DE::Render
 {
@@ -20,54 +21,113 @@ namespace S2DE::Render
 	void Texture::Cleanup()
 	{
 		Core::Release(m_resource);
-		Core::Release(m_texture_resource);
+		Core::Release(m_resourceview);
+		Core::Release(m_texture2d);
+		Core::Release(m_texture_sampler_state);
+	}
+
+	void Texture::UpdateTextureDesc()
+	{
+		m_resourceview->GetResource(&m_resource);
+		m_resource->QueryInterface<ID3D11Texture2D>(&m_texture2d);
+		m_texture2d->GetDesc(&m_texture_desc);
 	}
 
 	bool Texture::Load(std::string path)
 	{
+		//If path is empty
 		if (Core::Other::isStringEmpty(path))
+		{
+			Logger::Error("No path setted for texture!");
+			CreateEmptyTexture();
 			return false;
+		}
 
-		ID3D11Resource* res;
+		HRESULT hr;
 
-		S2DE_CHECK(D3DX11CreateShaderResourceViewFromFile(Core::Engine::GetRenderer()->GetDevice(), path.c_str(), NULL, NULL, &m_resource, NULL), "Can't create texture!");
+		//If file extension is a dds we are use CreateDDSTextureFromFile function
+		//if not we use CreateWICTextureFromFile
+		if (Core::Other::GetFileExtension(path) == ".dds")
+		{
+			hr = DirectX::CreateDDSTextureFromFile(Core::Engine::GetRenderer()->GetDevice(), Core::Other::StringToWString(path).c_str(), &m_resource, &m_resourceview);
+			if (FAILED(hr))
+			{
+				Logger::Error("Can't create dds texture from file %s %s", path.c_str(), Core::Utils::GetHRCodeDetails(hr));
+				CreateEmptyTexture();
+				return false;
+			}
+		}
+		else
+		{
+			hr = DirectX::CreateWICTextureFromFile(Core::Engine::GetRenderer()->GetDevice(), Core::Other::StringToWString(path).c_str(), &m_resource, &m_resourceview);
+			if (FAILED(hr))
+			{
+				Logger::Error("Can't create texture from file %s %s", path.c_str(), Core::Utils::GetHRCodeDetails(hr));
+				CreateEmptyTexture();
+				return false;
+			}
+		}
+		
+		//Set sampler state
+		D3D11_SAMPLER_DESC desc{};
 
-		m_resource->GetResource(&res);
-		res->QueryInterface<ID3D11Texture2D>(&m_texture_resource);
+		desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		desc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+		desc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+		desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+		desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+		desc.BorderColor[0] = 0;
+		desc.BorderColor[1] = 0;
+		desc.BorderColor[2] = 0;
+		desc.BorderColor[3] = 0;
+		desc.MinLOD = -FLT_MAX;
+		desc.MaxLOD = FLT_MAX;
+		desc.MipLODBias = 0.0f;
+		desc.MaxAnisotropy = 1;
 
-		m_texture_resource->GetDesc(&m_texture_desc);
+		S2DE_CHECK(Core::Engine::GetRenderer()->GetDevice()->CreateSamplerState(&desc, &m_texture_sampler_state), "Can't create sampler state");
 
-		Core::Release(res);
+		UpdateTextureDesc();
+		return true;
+	}
 
-		D3D11_SAMPLER_DESC samplerDesc;
+	bool Texture::CreateEmptyTexture()
+	{
+		//TODO
+		//Basic custom color change
+		//By default it's pink
+		static const uint32_t pixel = 0xffc99aff;
+		D3D11_SUBRESOURCE_DATA initData = { &pixel, sizeof(uint32_t), 0 };
 
-		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
 
-		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+		D3D11_TEXTURE2D_DESC desc { };
 
-		samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+		desc.Width = 1;
+		desc.Height = 1;
+		desc.MipLevels = 1;
+		desc.ArraySize = 1;
+		desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.SampleDesc.Count = 1;
+		desc.Usage = D3D11_USAGE_IMMUTABLE;
+		desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
 
-		samplerDesc.BorderColor[0] = 0;
-		samplerDesc.BorderColor[1] = 0;
-		samplerDesc.BorderColor[2] = 0;
-		samplerDesc.BorderColor[3] = 0;
+		ID3D11Texture2D* texture = nullptr;
+		S2DE_CHECK(Core::Engine::GetRenderer()->GetDevice()->CreateTexture2D(&desc, &initData, &texture), "Can't create empty texture");
 
-		samplerDesc.MinLOD = 0;
-		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
-		samplerDesc.MipLODBias = 0.0f;
+		D3D11_SHADER_RESOURCE_VIEW_DESC sdesc{};
 
-		samplerDesc.MaxAnisotropy = 0;
+		sdesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		sdesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+		sdesc.Texture2D.MipLevels = 1;
 
-		S2DE_CHECK(Core::Engine::GetRenderer()->GetDevice()->CreateSamplerState(&samplerDesc, &m_texture_sampler_state), "Can't create sampler state");
-
+		S2DE_CHECK(Core::Engine::GetRenderer()->GetDevice()->CreateShaderResourceView(texture, &sdesc, &m_resourceview), "Can't create resource (empty texture)");
+		UpdateTextureDesc();
 		return true;
 	}
 
 	void Texture::Bind(std::uint32_t NumViews)
 	{
-		Core::Engine::GetRenderer()->GetContext()->PSSetShaderResources(0, NumViews, &m_resource);
-		Core::Engine::GetRenderer()->GetContext()->PSGetSamplers(0, NumViews, &m_texture_sampler_state);
+		Core::Engine::GetRenderer()->GetContext()->PSSetSamplers(0, 1, &m_texture_sampler_state);
+		Core::Engine::GetRenderer()->GetContext()->PSSetShaderResources(0, NumViews, &m_resourceview);
 	}
 }
--- Engine/Graphics/Texture.h
+++ Engine/Graphics/Texture.h
@@ -13,9 +13,11 @@ namespace S2DE::Render
 		
 		virtual void					 Cleanup() override;
 		virtual bool					 Load(std::string path) override;
+		virtual bool					 CreateEmptyTexture();
 
-		inline ID3D11ShaderResourceView* GetShaderResource() const { return m_resource; }
-		inline ID3D11Texture2D*			 GetTexture2D() const { return m_texture_resource; }
+
+		inline ID3D11ShaderResourceView* GetShaderViewResource() const { return m_resourceview; }
+		inline ID3D11Texture2D*			 GetTexture2D() const { return m_texture2d; }
 		inline std::uint32_t			 GetWidth() const { return m_texture_desc.Width; }
 		inline std::uint32_t			 GetHeight() const { return m_texture_desc.Height; }
 		inline std::uint32_t			 GetMipLevels() const { return m_texture_desc.MipLevels; }
@@ -25,9 +27,13 @@ namespace S2DE::Render
 
 		void							 Bind(std::uint32_t NumViews = 1);
 	private:
-		ID3D11ShaderResourceView*		 m_resource;
-		ID3D11Texture2D*				 m_texture_resource;
+		ID3D11ShaderResourceView*		 m_resourceview;
+		ID3D11Resource*					 m_resource;
+
+		ID3D11Texture2D*				 m_texture2d;
 		D3D11_TEXTURE2D_DESC			 m_texture_desc;
 		ID3D11SamplerState*				 m_texture_sampler_state;
+
+		void							 UpdateTextureDesc();
 	};
 }
