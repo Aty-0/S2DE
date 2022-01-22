abea3b10c611db75de46a2123792070213be28ae
Antd23rus <danich-anton-live@yandex.ru>
2022-01-22 20:22:21 +0300

    Added safe check

--- Engine/Base/Utils/Logger.h
+++ Engine/Base/Utils/Logger.h
@@ -7,7 +7,8 @@
 #define S2DE_FATAL_ERROR(message) Logger::Fatal("%s\nTechnical information:\nLine:%d\nFile:%s\nin Function: %s" ,message , __LINE__, __FILE__ , __func__); 
 #define S2DE_NO_IMPL() Logger::Warning("No function implementation %s", __func__);
 #define S2DE_ASSERT(a) if(!(a)) { Logger::Fatal("Assertion failed %s\n File:%s\n Line:%d", #a,  __FILE__, __LINE__); }
-#define S2DE_CHECK(f, r) if (!CheckHR(f)) { S2DE_FATAL_ERROR(r); return false; }  
+#define S2DE_CHECK(f, r) if (!CheckHR(f, true)) { S2DE_FATAL_ERROR(r); return false; }  
+#define S2DE_CHECK_SAFE(f, r) if (!CheckHR(f, true)) { Logger::Error("%s %s %d %s", #f,  __FILE__, __LINE__, r); return false; }  
 
 namespace S2DE::Core::Utils
 {
@@ -29,24 +30,30 @@ namespace S2DE::Core::Utils
 		static const std::string GetLogFileName() { return m_log_file_name; }
 
 	private:
-		static void				Print(const char* type, const char* text);
-		static std::string		GetCorrentTime();
-
-		static std::uint32_t	m_linecount;
-		static time_t           m_time;
-		static tm*				m_localtime;
-		static std::ofstream	m_log_file;
-		static std::string		m_log_file_name;
+		static void				 Print(const char* type, const char* text);
+		static std::string		 GetCorrentTime();
+								 
+		static std::uint32_t	 m_linecount;
+		static time_t            m_time;
+		static tm*				 m_localtime;
+		static std::ofstream	 m_log_file;
+		static std::string		 m_log_file_name;
 	};
 
-	static inline bool CheckHR(HRESULT hr)
+	static inline std::string GetHRCodeDetails(HRESULT hr)
+	{
+		_com_error err(hr);
+		return std::string(err.ErrorMessage());
+	}
+
+	static inline bool CheckHR(HRESULT hr, bool print_code = false)
 	{
 		if (FAILED(hr))
 		{
-			_com_error err(hr);
-			Logger::Error("HRESULT failed! Error Code: 0x%08X Details %s ", hr, err.ErrorMessage());
+			if(print_code == true)
+				Logger::Error("Error Code: 0x%08X Details: %s ", hr, GetHRCodeDetails(hr).c_str());
+
 			return false;
-	
 		}
 	
 		return true;
