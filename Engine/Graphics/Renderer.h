f2f52025b653af1f30f8751af7fee6b3bbd148e3
Antd23rus <danich-anton-live@yandex.ru>
2022-01-22 20:44:21 +0300

    Big update

--- Engine/Graphics/Renderer.h
+++ Engine/Graphics/Renderer.h
@@ -7,12 +7,24 @@
 #include "Libs/imgui/imgui_impl_win32.h"
 #include "Libs/imgui/imgui_impl_dx11.h"
 
+//This macro enable debug layers
+//Disable optimization and enable debug mode in shaders
+#define S2DE_DEBUG_RENDER_MODE 
+
 //ImGui WndProcHandler
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 //Name of default font for ui_basic_text and imgui  
 #define S2DE_DEFAULT_FONT_NAME "default"
 
+namespace S2DE
+{
+	namespace Editor
+	{
+		class EditorCursor;
+		class EditorCenterCursor;
+	}
+}
 
 namespace S2DE::Render
 {
@@ -32,16 +44,30 @@ namespace S2DE::Render
 		bool						Reset();
 		void						Destroy();
 		void						Render();
-
-		inline ID3D11Device*		GetDevice() const { return m_device; }
-		inline ID3D11DeviceContext* GetContext() const { return m_deviceContext; }
-		inline IDXGISwapChain*		GetSwapChain() const { return m_swapChain; }
+		
+		inline ID3D11Device*		GetDevice() { return m_device; }
+		inline ID3D11DeviceContext* GetContext() { return m_deviceContext; }
+		inline IDXGISwapChain*		GetSwapChain() { return m_swapChain; }
+		inline ID3D11RenderTargetView* GetRenderTargetView() { return m_renderTargetView; }
+		inline IDXGIFactory1*		GetFactory() { return m_factory1; }
 		inline bool					GetVsync() const { return m_vsync; }
 		inline D3D11_VIEWPORT		GetViewport() const { return m_viewport; }
+
+		inline ID3D11Texture2D*			GetDepthStencilBuffer() { return m_depthStencilBuffer; }
+		inline ID3D11DepthStencilState* GetDepthStencilState() { return   m_depthStencilState; }
+		inline ID3D11DepthStencilView* GetDepthStencilView() { return    m_depthStencilView; }
+
 		inline ID3D11ShaderResourceView* GetFramebufferShaderResource() const { return m_framebuffer_shaderResourceView; }
 		inline ID3D11Texture2D*			 GetFramebufferTextureData() const { return m_framebuffer_data; }
+
 		void						SwitchFillMode(RenderFillMode mode);
 		void						SetBackColor(Math::Color<float> color);
+
+		void						TurnZBufferOff();
+		void						TurnZBufferOn();
+
+		void						ToggleImGuiWindowsVisible();
+		void						ToggleImGuiDemoWindowVisible();
 
 	private:					 
 		bool						InitImGui();
@@ -49,33 +75,40 @@ namespace S2DE::Render
 		void						RenderImGui();
 		void						LoadCustomImguiTheme();
 		void						DestroyImGui();
-		bool						GetDisplay(std::uint32_t& mode_numerator, std::uint32_t& mode_denominator);
+		//bool						GetDisplay(std::uint32_t& mode_numerator, std::uint32_t& mode_denominator);
 		bool						CreateDeviceAndSwapChain();
 		bool						CreateDepthStencil();
 		void						UpdateViewport();
 		void						Clear();
 		void						End();
+		bool						CaptureMessages();
 		bool						CreateRasterizerState();
 		bool						CreateBlendState();
 		bool						CreateFramebufferTexture(ID3D11Texture2D* sw_buff);
 		void						UpdateFramebufferShaderResource();
 
-
+		IDXGIAdapter*				m_adapter;
 		IDXGISwapChain*				m_swapChain;
 		ID3D11Device*				m_device;
 		ID3D11DeviceContext*		m_deviceContext;
 		ID3D11RenderTargetView*		m_renderTargetView;
 		ID3D11RasterizerState*		m_rasterState;
 		ID3D11BlendState*			m_blendState;
-		VideoCardDesc				m_videocard_desc;
+		
+
+		IDXGIFactory1*				m_factory1;
+		ID3D11Device1*				m_device1;
+		ID3D11DeviceContext1*		m_deviceContext1;
+		IDXGISwapChain1*			m_swapChain1;
+
 
 		//Depth strencil
 		ID3D11Texture2D*			m_depthStencilBuffer;
 		ID3D11DepthStencilState*	m_depthStencilState;
+		ID3D11DepthStencilState*	m_depthDisabledStencilState;
 		ID3D11DepthStencilView*		m_depthStencilView;
 
 		bool						m_vsync;
-		D3D_FEATURE_LEVEL			m_feature_level;
 		std::uint32_t				m_mode_numerator;
 		std::uint32_t				m_mode_denominator;
 
@@ -83,7 +116,7 @@ namespace S2DE::Render
 
 		std::uint32_t				m_device_flag;
 
-		Math::Color<float>				m_clearColor;
+		Math::Color<float>			m_clearColor;
 
 		std::vector<std::pair<std::string, class ImGui_Window*>>	m_windows_storage;
 
@@ -91,6 +124,15 @@ namespace S2DE::Render
 
 		ID3D11Texture2D*			m_framebuffer_data;
 		ID3D11ShaderResourceView*	m_framebuffer_shaderResourceView;
+
+		ID3D11Debug*				m_d3ddebug;
+		ID3D11InfoQueue*			m_d3dinfoqueue;
+
+		class Editor::EditorCursor*	m_editor_cursor;
+		class Editor::EditorCenterCursor*	m_editor_center_cursor;
+
+		bool						m_show_imgui_windows;
+		bool						m_show_imgui_demo_wnd;
 
 	public:  
 		inline class ImGui_Window*	GetImGui_Window(std::string name) const;
