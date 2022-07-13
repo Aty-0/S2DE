899fda397a2abe9fc65bdd56d146decc29b8a25a
A_T <54220849+Antd23rus@users.noreply.github.com>
2022-07-13 12:15:08 +0300

    Fix build

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,5 +1,6 @@
 #include "Application.h"
 #include <Base\CoreMinimal.h>
+#include <GameObjects\NoTextureTestObject.h>
 #include <GameObjects\TestObject.h>
 
 
@@ -10,7 +11,8 @@ using namespace S2DE::Math;
 using namespace S2DE::Render;
 using namespace S2DE::Core;
 using namespace S2DE::Core::Other;
-using namespace S2DE::Core::Debug;
+using namespace DirectX::SimpleMath;
+
 
 
 Application::Application()
@@ -19,6 +21,7 @@ Application::Application()
 
 
 	engine->RunEngineInEditorMode(this);
+	//engine->RunEngineInGameMode(this);
 }
 
 Application::~Application()
@@ -26,11 +29,9 @@ Application::~Application()
 
 }
 
+
 void Application::OnStart()
 {
-	//Create 5 test objects
-	for(std::int32_t i = 0; i <= 5; i++)
-		CreateGameObject<Sprite>("testobject", "GameObject", 1, Vector3(i * 2, 0, i));
 
 }
 
@@ -39,75 +40,19 @@ bool Application::LoadResources()
 	return true;
 }
 
-
 void Application::OnUpdate(float DeltaTime)
-{
-
-}
-
-void Application::OnRender()
 {
 	
 }
 
+void Application::OnRender()
+{
+
+}
+
 void Application::InputEvents()
 {
-	if (Engine::GetInputManager()->IsKeyDown(KeyCode::KEY_Y))
-		Engine::GetSceneManager()->GetScene()->Delete("input_obj");
-
-	if (Engine::GetInputManager()->IsKeyDown(KeyCode::KEY_T))
-	{
-		Vector3 pos = GetObjectByName<Camera>(S2DE_MAIN_CAMERA_NAME)->GetPosition();
-		static std::int32_t c = 0;
-		c++;
-		CreateGameObject<Sprite>("input_obj" + std::to_string(c), "GameObject", 1, Vector3(pos.x, pos.y, c));
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->SetAtlasSize(Vector2(33, 60));
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->SetAtlasFramePosition(0, 0);
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->LoadTexture("Player");
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->SetScale(Vector2(2.0f, 2.0f));
-
-	}
-
-
-	if (Engine::GetInputManager()->IsKeyDown(KeyCode::KEY_U))
-	{
-		Vector3 pos = GetObjectByName<Camera>(S2DE_MAIN_CAMERA_NAME)->GetPosition();
-		static std::int32_t d = 0;
-		d++;
-		CreateGameObject<Sprite>("inputd_obj" + std::to_string(d), "GameObject", 1, Vector3(pos.x, pos.y, d));
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->SetAtlasSize(Vector2(33, 60));
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->SetAtlasFramePosition(0, 0);
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->LoadTexture("Player2");
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->SetScale(Vector2(2.0f, 2.0f));
-
-	}
-
-	if (Engine::GetInputManager()->IsKeyPressed(KeyCode::KEY_P))
-	{
-		Vector3 pos = GetObjectByName<Camera>(S2DE_MAIN_CAMERA_NAME)->GetPosition();
-		static std::int32_t c = 0;
-		c++;
-		CreateGameObject<Sprite>("input_obj" + std::to_string(c), "GameObject", 1, Vector3(pos.x, pos.y, c));
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->SetAtlasSize(Vector2(33, 60));
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->SetAtlasFramePosition(0, 0);
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->LoadTexture("Player");
-		GetObjectByName<Sprite>("input_obj" + std::to_string(c))->SetScale(Vector2(2.0f, 2.0f));
-
-	}
-
-
-	if (Engine::GetInputManager()->IsKeyPressed(KeyCode::KEY_O))
-	{
-		Vector3 pos = GetObjectByName<Camera>(S2DE_MAIN_CAMERA_NAME)->GetPosition();
-		static std::int32_t d = 0;
-		d++;
-		CreateGameObject<Sprite>("inputd_obj" + std::to_string(d), "GameObject", 1, Vector3(pos.x, pos.y, d));
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->SetAtlasSize(Vector2(33, 60));
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->SetAtlasFramePosition(0, 0);
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->LoadTexture("Player2");
-		GetObjectByName<Sprite>("inputd_obj" + std::to_string(d))->SetScale(Vector2(2.0f, 2.0f));
-
-	}
+	
 }
 
 void Application::OnResetDevice()
