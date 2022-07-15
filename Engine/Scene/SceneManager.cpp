248eb2b0b57d9d5fb9ca7045697fab13c37ccdcf
A_T <54220849+Antd23rus@users.noreply.github.com>
2022-07-15 12:16:41 +0300

    UI draw stage, added EditorCenterCursor creation

--- Engine/Scene/SceneManager.cpp
+++ Engine/Scene/SceneManager.cpp
@@ -5,7 +5,10 @@
 #include "Graphics/Renderer.h"
 
 #include "GameObjects/Skybox.h"
+#include "GameObjects/UI/UI_Drawable.h"
+
 #include "Editor/EditorGrid.h"
+#include "Editor/EditorCenterCursor.h"
 
 #include <boost/range/adaptor/reversed.hpp>
 
@@ -30,9 +33,11 @@ namespace S2DE::Scene
 		m_scene = new Scene();
 		CreateGameObject<GameObjects::Camera>(S2DE_MAIN_CAMERA_NAME, S2DE_ENGINE_GAMEOBJECT_TYPE, -1);
 		CreateGameObject<GameObjects::Skybox>("Skybox", "");
+
 		if (Core::Engine::isEditor())
 		{
 			CreateGameObject<Editor::EditorGrid>("_EditorGrid", S2DE_ENGINE_GAMEOBJECT_TYPE, -1, DirectX::SimpleMath::Vector3(GRID_CELLS / 2, -1, GRID_CELLS / 2));
+			CreateGameObject<Editor::EditorCenterCursor>("_UIEditorCenterCursor", S2DE_ENGINE_GAMEOBJECT_TYPE, -1);
 		}
 	}	 
 		 
@@ -83,19 +88,34 @@ namespace S2DE::Scene
 		{
 			for (const auto& object : m_scene->GetStorage())
 			{
-				auto GameObject = object.second.get();
-				if (!GameObject->Alpha)
-					GameObject->Render();
+				auto gameObject = object.second.get();
+				if (!gameObject->Alpha && gameObject != dynamic_cast<GameObjects::UI::UI_Drawable*>(gameObject))
+				{
+					gameObject->Render();
+				}
 			}
 		
 			Core::Engine::GetRenderer()->TurnOnAlphaBlending();
 			for (const auto& object : boost::adaptors::reverse(m_scene->GetStorage()))
 			{
-				auto GameObject = object.second.get();
-				if (GameObject->Alpha)				
-					GameObject->Render();
+				auto gameObject = object.second.get();
+				if (gameObject->Alpha && gameObject != dynamic_cast<GameObjects::UI::UI_Drawable*>(gameObject))
+				{
+					gameObject->Render();
+				}
 			}
 			Core::Engine::GetRenderer()->TurnOffAlphaBlending();
+
+			Core::Engine::GetRenderer()->TurnZBufferOff();
+			for (const auto& object : m_scene->GetStorage())
+			{
+				auto gameObject = object.second.get();
+				if (gameObject == dynamic_cast<GameObjects::UI::UI_Drawable*>(gameObject))
+				{
+					gameObject->Render();
+				}
+			}
+			Core::Engine::GetRenderer()->TurnZBufferOn();
 		}
 	}
 
